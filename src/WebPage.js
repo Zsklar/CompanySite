c7d5b400a35f5344b9d4ac7aa62019b0c2305f28
Zach Sklar <zacharysklar120@gmail.com>
2023-02-02 17:14:41 -0500

    Testing mobile

--- src/WebPage.js
+++ src/WebPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import NavBar from './NavBar';
+import {BrowserView, MobileView} from 'react-device-detect';
 
 class WebPage extends React.Component {
   render() {
-    return <NavBar/>;
+    return <div>
+      <BrowserView>
+        <NavBar/>
+      </BrowserView>
+      <MobileView>
+        <h1>YOU ARE ON MOBILE</h1>
+      </MobileView>
+    </div>
   }
 }
 
