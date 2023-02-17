2e5580d4f074399a0b2d085fa83199b2b4cf6987
Zach Sklar <zacharysklar120@gmail.com>
2023-02-16 19:05:02 -0500

    Added about text

--- src/About.js
+++ src/About.js
@@ -4,7 +4,20 @@ import React from 'react'
 class About extends React.Component{
  render(){
     return <div>
-      <h1>About us page</h1>
+      <h3>Our Philosophy:
+
+At Artemesia Labs, we look to increase access to markets for retail and institutional investors by providing better prices, more liquidity, and the tools necessary to compete in the open market.
+
+Artemesia Finance
+
+Market Making:
+Our teams develop and deploy market-neutral strategies that provide liquidity to traders and investors on decentralized and centralized markets. Our strategies include CeFi/CeFi arbitrage, Defi/CeFi arbitrage, and DeFi/DeFi arbitrage, including cross-chain informational arbitrage.
+Directional Investing:
+At this time our team doesn't actively invest in directional strategies, but we do provide research and analyses of protocols and systems for firms and investors who are planning to invest directionally.
+
+Venture
+
+Our Venture team are experts in early-stage web3 startups. We use our network and expertise to help founders build teams, make important design decisions, market their products, and connect to investors. We also offer entrepreneurs access to our in-house tech team to help them quickly break through roadblocks that require specialized expertise.</h3>
     </div>
  }
 }
