f48f50823aa4a7ca075891661784a61dd9fe50dd
Zach Sklar <zacharysklar120@gmail.com>
2023-02-22 22:40:27 -0500

    Homepage updates

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,42 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
+
 import styles from './style.module.css'
 import banner from './Images/banner.jpg';
-import logo from './Images/Logo-Color.png';
+import sky from './Images/Skyline.jpg';
 
 class Home extends React.Component{
  render(){
    return <div>
+   <Fade cascade>
    <img className={styles.homeImg} src={banner} alt="banner"></img>
    <div className={styles.homeBlock}>
+      <Fade left>
       <h1 className={styles.homeTitle}>At Ruatan Capital</h1>
-      <h2 className={styles.homeMission}>We're building the best infrastructure for the new world of finance.  
-         We're striving to bring anyone with a viable business model to market.
-      </h2>
-      <img className={styles.homeImg2} src={logo} alt="logo"></img>
+      </Fade>
+         <h2 className={styles.homeMission}>We're building the best infrastructure for the new world of finance.  
+            We're striving to bring anyone with a viable business model to market.
+         </h2>
+      <Fade right>
+         <img className={styles.homeImg2} src={sky} alt="logo"></img>
+      </Fade>
    </div>
 
    <div className={styles.homeMethodSpan}>
-      <h2 className={styles.homeMethodTitle}>Our Methodology</h2>
+      <Fade bottom>
+         <h2 className={styles.homeMethodTitle}>Our Methodology</h2>
+      </Fade>
+      <div className={styles.homeMethodCon}>
+         <div className={styles.homeMethodBar}></div>
+      </div>
       <h3 className={styles.homeMethod}>There are many roadblocks preventing products from getting to market: 
       technology needs, lack of access to financing, regulatory blocks from accessing certain markets, and many others. 
       </h3>
       <h3 className={styles.homeMethod2}>At Ruatan Capital, we're experts at navigating the world of early-stage product and business development, from providing 
       access to markets to building adequate teams to raising capital.</h3>
    </div>
- </div>
+   </Fade>
+   </div>
  }
 }
 
